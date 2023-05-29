997b54fdb304ee6e6042f6f62206639090186b72
Oliver Lehmann <lehmannoliver96@gmail.com>
2023-05-29 10:23:37 +0700

    initial commit

---
+++ video_player.js
@@ -0,0 +1,34 @@
+var player = videojs('BAR-RMA');
+
+player.on('play', function() {
+  console.log('the video is playing');
+});
+
+player.on('pause', function() {
+  console.log('the video is paused');
+});
+
+// Add multiple audio tracks with the audioTracks API
+var player = videojs('my-video');
+
+var track1 = new videojs.AudioTrack({
+  id: 'track1',
+  kind: 'alternative',
+  label: 'English',
+  language: 'en'
+});
+
+var track2 = new videojs.AudioTrack({
+  id: 'track2',
+  kind: 'alternative',
+  label: 'Spanish',
+  language: 'es'
+});
+
+player.audioTracks().addTrack(track1);
+player.audioTracks().addTrack(track2);
+
+// to switch between audio tracks, we use the enabled property
+track1.enabled = true; // enable track1
+track2.enabled = false; // disable track2
+
